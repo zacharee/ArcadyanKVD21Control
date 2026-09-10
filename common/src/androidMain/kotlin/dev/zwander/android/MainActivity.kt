e1cf6de380f6a7551de74a2d5d14d24c4d12fe15
Zachary Wander <zachary.wander@gmail.com>
2026-09-10 11:15:21 -0400

    Add ACCESS_LOCAL_NETWORK permission grant process for Android 17

--- common/src/androidMain/kotlin/dev/zwander/android/MainActivity.kt
+++ common/src/androidMain/kotlin/dev/zwander/android/MainActivity.kt
@@ -1,33 +1,67 @@
 package dev.zwander.android
 
+import android.annotation.SuppressLint
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetProperties
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.resources.compose.stringResource
 import dev.zwander.common.App
 import dev.zwander.common.model.SettingsModel
 import dev.zwander.common.widget.ConnectionStatusWidgetReceiver
+import dev.zwander.resources.common.MR
+import dev.zwander.resources.common.close_app
+import dev.zwander.resources.common.grant
+import dev.zwander.resources.common.local_network_permission
+import dev.zwander.resources.common.local_network_permission_desc
 import io.github.vinceglb.filekit.FileKit
 import io.github.vinceglb.filekit.dialogs.init
 
 class MainActivity : AppCompatActivity() {
+    private var showingLocalNetworkAccessDialog by mutableStateOf(false)
     private val appWidgetManager by lazy { getSystemService(APPWIDGET_SERVICE) as AppWidgetManager }
+    private val requestPermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+        if (!granted) {
+            finish()
+        } else {
+            showingLocalNetworkAccessDialog = false
+        }
+    }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
@@ -55,10 +89,70 @@ class MainActivity : AppCompatActivity() {
                 updateWidgetRefresh()
             }
 
+            LaunchedEffect(null) {
+                showingLocalNetworkAccessDialog = !hasLocalNetworkAccess()
+            }
+
             App(
                 modifier = Modifier.imePadding(),
                 fullPadding = WindowInsets.systemBars.only(WindowInsetsSides.Horizontal).asPaddingValues(),
             )
+
+            if (showingLocalNetworkAccessDialog) {
+                ModalBottomSheet(
+                    onDismissRequest = { showingLocalNetworkAccessDialog = false },
+                    sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+                    sheetGesturesEnabled = false,
+                    dragHandle = null,
+                    properties = ModalBottomSheetProperties(
+                        shouldDismissOnBackPress = false,
+                        shouldDismissOnClickOutside = false,
+                    ),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(
+                            top = 32.dp,
+                            start = 16.dp,
+                            end = 16.dp,
+                            bottom = 16.dp,
+                        ).fillMaxWidth(),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        Text(
+                            text = stringResource(MR.strings.local_network_permission),
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+
+                        Text(
+                            text = stringResource(MR.strings.local_network_permission_desc),
+                        )
+
+                        Row(
+                            modifier = Modifier.align(Alignment.End),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            TextButton(
+                                onClick = {
+                                    finish()
+                                },
+                            ) {
+                                Text(text = stringResource(MR.strings.close_app))
+                            }
+
+                            TextButton(
+                                onClick = {
+                                    requestPermissionsLauncher.launch(
+                                        @SuppressLint("InlinedApi")
+                                        android.Manifest.permission.ACCESS_LOCAL_NETWORK,
+                                    )
+                                },
+                            ) {
+                                Text(text = stringResource(MR.strings.grant))
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -68,5 +162,11 @@ class MainActivity : AppCompatActivity() {
         if (appWidgetManager.getAppWidgetIds(ComponentName(this, ConnectionStatusWidgetReceiver::class.java)).isNotEmpty()) {
             App.instance.scheduleWidgetRefresh()
         }
+    }
+
+    private fun hasLocalNetworkAccess(): Boolean {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.CINNAMON_BUN ||
+                checkCallingOrSelfPermission(android.Manifest.permission.ACCESS_LOCAL_NETWORK) ==
+                PackageManager.PERMISSION_GRANTED
     }
 }
